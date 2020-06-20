d4fc78e5a5e45575613bfb135808bc1e599b9027
上野誉司 <isacatoneu@uenohomareshinoMacBook-puro.local>
2020-06-20 21:22:24 +0900

    add_header

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,16 +1,35 @@
 class DiariesController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @diaries =Diary.all
   end
 
   def show
   end
 
-  def edix
-  end
+
 
   def edit
   end
 
   def new
+    @diary =Diary.new
   end
+
+  def create
+    @diary =Diary.new(diary_params)
+    @diary.user_id=current_user.id
+    if @diary.save
+    redirect_to diaries_path
+    flash[:notice] ="投稿しました"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:artist_name,:live_name,:title,:impression,:date,:user_id,:setlist)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#top'
-  get 'home/top'
+  root 'diaries#index'
   resources :users, only: [:show, :edit, :update]
-  resources :diaries, only: [:index,:show,:edit,:update,:destroy,:create]
+  resources :diaries, only: [:index,:show,:edit,:new,:update,:destroy,:create]
 
 end
